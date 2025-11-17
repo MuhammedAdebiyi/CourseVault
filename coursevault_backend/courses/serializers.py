755dfe8e49a979267abcfc49310cb8c9180ff948
Muhammed Adebiyi <muhammedadebiyi2006@gmail.com>
2025-11-18 00:45:09 +0100

    Updated the backend

--- coursevault_backend/courses/serializers.py
+++ coursevault_backend/courses/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
 from .models import Folder, PDF
 from .utils import generate_presigned_url
-class PDFSerializer(serializers.ModelSerializer):
 
+class PDFSerializer(serializers.ModelSerializer):
     download_url = serializers.SerializerMethodField()
 
     class Meta:
         model = PDF
         fields = "__all__"
 
+    def validate_file(self, value):
+        if not value.name.lower().endswith('.pdf'):
+            raise serializers.ValidationError("Only PDF files are allowed")
+        return value
 
     def get_download_url(self, obj):
         if obj.file:
             return generate_presigned_url(obj.file.name, expires_in=3600)
         return None
+
 
 class FolderSerializer(serializers.ModelSerializer):
     pdfs = PDFSerializer(many=True, read_only=True)
